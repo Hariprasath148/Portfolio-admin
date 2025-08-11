02b5f017ac54ef605c2e4efe92638239ec33e1a9
Hariprasath148 <janhharigithub@gmail.com>
2025-08-11 12:43:04 +0530

    bg change

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -7,9 +7,9 @@ export const AuthLayout = () => {
     <div className='auth-wrapper w-100 h-100'>
         <div className="auth-container p-2 row m-2">
             <div className="col-lg-4 col-12 auth-info-container pt-3 px-3">
-              <p className='m-0 mb-1 text-light'>Access Restricted</p>
-              <p className='m-0 mt-2 text-light'>This page is accessible only by me. If you'd like to know more about me, click the link below.</p>
-              <a href="https://hariprasath-profile-sand.vercel.app/" className="text-light mt-1 d-block" target="_blank" >Know more</a>
+              <p className='m-0 mb-1'>Access Restricted</p>
+              <p className='m-0 mt-2'>This page is accessible only by me. If you'd like to know more about me, click the link below.</p>
+              <a href="https://hariprasath-profile-sand.vercel.app/" className="text-dark mt-1 d-block" target="_blank" >Know more</a>
             </div>
             <div className="col-lg-8 col-12 px-lg-5">
                 <Outlet />
